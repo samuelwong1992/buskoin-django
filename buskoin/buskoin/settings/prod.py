8767aaa9c6c1e7c8546b913d9bd008587bbd78cf
Sam <samuelwong1992@gmail.com>
2021-04-27 06:39:11 +0000

    updating settings for domain name

--- buskoin/buskoin/settings/prod.py
+++ buskoin/buskoin/settings/prod.py
@@ -1,11 +1,12 @@
 from buskoin.settings.settings import *
 
-DEBUG=False
+DEBUG=True
 
 ALLOWED_HOSTS = [
     '128.199.184.32',
 	'localhost',
 	'*.buskoin.com',
+	'buskoin.com',
 ]
 
 BASE_API_URL = "http://128.199.184.32"
